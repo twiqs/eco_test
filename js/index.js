4a08ecd3d9f3f69bd35489cb3e5114a33a0892f1
Adelina <adelinaakylbekova565@gmail.com>
2024-03-18 13:21:21 +0600

    add change for btn in sec form

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 const btn = document.querySelector('.form_btn')
 const checkbox = document.getElementById('autoCheckbox');
+const btnChangeAutobtnChangeAuto = document.getElementById('change-btn');
 const access = {
     "Admin": "123",
     "Adelina": "qwerty",
@@ -22,8 +23,8 @@ console.log()
 btn.addEventListener('click', function () {
     let inputLogin = document.querySelector('.input__login').value
     let inputPsw = document.querySelector('.input__psw').value
-    
-    
+
+
 
 
 
@@ -34,12 +35,12 @@ btn.addEventListener('click', function () {
                 document.querySelector('.success').style.display = "block"
                 document.querySelector('.js-name').innerText = inputLogin
                 document.querySelector('.form').style.display = "none"
-    
+
             } else {
                 document.querySelector('.input_txt__psw').style.display = "block"
             }
         }
-        
+
     } else {
         document.querySelector('.input_txt__login').style.display = "block"
 
@@ -47,4 +48,16 @@ btn.addEventListener('click', function () {
 
 
 
+});
+const formSignUp = document.querySelector('sign_up')
+const formSignIn = document.querySelector('sign_in')
+
+btnChangeAuto.addEventListener('click', function () {
+    if (this.classList.contains('open')) {
+        this.classList.remove('open')
+        // text.style.fontWeight = 400
+    } else{
+        this.classList.add('open')
+        // text.style.fontWeight = 700
+    }
 })
