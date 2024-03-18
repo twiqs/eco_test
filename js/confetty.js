caad1b5c463f31ea84da02af80ce443e00ec306e
Adelina <adelinaakylbekova565@gmail.com>
2024-03-18 13:29:48 +0600

    del unneed file

--- js/confetty.js
+++
@@ -1,5 +0,0 @@
-// import confetti from 'https://cdn.skypack.dev/canvas-confetti';
-// function party() {
-//     confetti()
-// }
-// document.getElementById('confetti').addEventListener('click', party)
