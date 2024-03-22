9765f21bfea2764a393c7acd7812405fca56a3c6
Aelina00 <sugar.w.0iiol@gmail.com>
2024-03-22 06:16:33 +0600

    added swiper js code

--- js/metal.js
+++ js/metal.js
@@ -54,6 +54,34 @@
 })(jQuery);
 
 
+// swiper
 
-
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  spaceBetween: 60,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+const constraints = {
+  video: true
+};
 
